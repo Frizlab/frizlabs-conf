a8982096e341ab2ae2f4a814eed9f8d16913e793
Frizlab <fload@me.com>
2019-03-07 11:37:46 +0100

    Install Python3 automatically on Darwin if not already there

---
+++ roles/bin/files/local_brew_install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -euo pipefail
+
+# ###################################### #
+#  Install the given packages if needed  #
+# ###################################### #
+
+
+# TODO: Add a way to specify the Homebrew package name for a given package. E.g.
+#       if you want to install realpath, you have to install coreutils.
+#       A possible syntax would be
+#          local_brew_install homebrew_path realpath,coreutils
+#       Indeed if the package name is the same as the executable name, there
+#       would be no need to specify both.
+usage() {
+	echo "$0 homebrew_instance_folder package1 ..."
+	echo ""
+	echo "The script will check if all packages can be found in the path. If at"
+	echo "least one package is not found, will try and install all the packages"
+	echo "from a “local” homebrew install, which will be done in the given"
+	echo "folder."
+}
+
+if [ "$#" -lt 2 ]; then
+	usage >/dev/stderr
+	exit 1
+fi
+
+readonly LOCAL_HOMEBREW_INSTALL="$1"
+shift
+
+export PATH="${PATH}:${LOCAL_HOMEBREW_INSTALL}/bin"
+export BREW="${LOCAL_HOMEBREW_INSTALL}/bin/brew"
+
+do_install=0
+for p in "$@"; do
+	if ! which -s "$p"; then
+		do_install=1
+		break
+	fi
+done
+if [ "$do_install" != "0" ]; then
+	if [ "$(uname)" != "Darwin" ]; then
+		echo "Shyingly not trying Homebrew when not on macOS for now…" >/dev/stderr
+		exit 2
+	fi
+	if [ ! -x "$BREW" ]; then
+		mkdir -p "${LOCAL_HOMEBREW_INSTALL}"
+		curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "${LOCAL_HOMEBREW_INSTALL}"
+	fi
+	if [ ! -x "$BREW" ]; then
+		echo "There was an issue installing a local Homebrew instance!" >/dev/stderr
+		exit 2
+	fi
+	
+	export HOMEBREW_NO_ANALYTICS=1
+	"$BREW" update
+	"$BREW" install "$@"
+fi
