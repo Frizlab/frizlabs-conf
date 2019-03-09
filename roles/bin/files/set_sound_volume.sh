06a18205fb18c8523eead94d95afa1759cdf3228
Frizlab <fload@me.com>
2019-03-09 13:59:22 +0100

    Add the set_sound_volume script

---
+++ roles/bin/files/set_sound_volume.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# ############################################### #
+#  Set system sound volume on macOS w/ osascript  #
+# ############################################### #
+
+
+if [ "$1" = "--help" -o "$1" = "help" -o -z "$1" -o "$1" -lt 0 -o "$1" -gt 100 ]; then
+	echo "Usage: $0 new_volume" >/dev/stderr
+	echo "   new_volume must be between 0 and 100" >/dev/stderr
+	exit 1
+fi
+
+new_volume="$(echo "($1 * 8) / 100" | bc)"
+osascript -e "tell application \"System Events\" to set volume $new_volume"; # Max is 8, min is 0
