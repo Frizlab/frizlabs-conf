7d18ab719d0c4e5dc09d21ac6a29183594c6eaac
Frizlab <fload@me.com>
2019-05-07 19:28:33 +0200

    Add support for normal user test in Debian

--- tests/test-debian-stretch.sh
+++ tests/test-debian-stretch.sh
@@ -15,6 +15,12 @@ test -f "$VAULT_ID_PATH" || ( echo "vault-id is not setup; aborting" && exit 1 )
 cp -f "$VAULT_ID_PATH" "$ASSETS_INPUTS_FOLDER/.vault-id"
 
 # Setting up the ansible group
-echo "home" >"$ASSETS_INPUTS_FOLDER/ansible_group"
-docker build . -f "debian-stretch/Dockerfile.root" -t frizlab-home-debian:stretch
-docker run --rm frizlab-home-debian:stretch "test-frizlabs-conf.sh"
+env=home
+base_image_name=debian
+base_image_tag=stretch-slim
+final_image_name="frizlab-$env-$base_image_name:$base_image_tag"
+builder_image_name="frizlab-conf-builder-$base_image_name:$base_image_tag"
+echo "$env" >"$ASSETS_INPUTS_FOLDER/ansible_group"
+docker build . -f "debian-stretch/Dockerfile.base" --build-arg BASE_IMAGE_NAME="$base_image_name" --build-arg BASE_IMAGE_TAG="$base_image_tag" -t "$builder_image_name"
+docker build . -f "debian-stretch/Dockerfile.user" --build-arg BASE_IMAGE="$builder_image_name" -t "$final_image_name"
+docker run --rm "$final_image_name" "test-frizlabs-conf.sh"
