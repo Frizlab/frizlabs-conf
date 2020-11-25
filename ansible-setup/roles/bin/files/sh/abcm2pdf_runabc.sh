b3ea8574052900bbcdda778e24266a360042e2de
Frizlab <fload@me.com>
2020-11-25 08:23:14 +0100

    Fix abcm2pdf_runabc to use abc2 instead of abc2.sh

--- ansible-setup/roles/bin/files/sh/abcm2pdf_runabc.sh
+++ ansible-setup/roles/bin/files/sh/abcm2pdf_runabc.sh
@@ -7,7 +7,7 @@ base=$(basename $1 "")
 format=$HOME/Documents/_en_cours/musique/abc/formats/a4_s750.fmt
 nom_s=apprentissage.pdf
 
-abc2.sh -pdf + -abcoptions "-F $format" -noop -- $base
+abc2 -pdf + -abcoptions "-F $format" -noop -- $base
 gv X.tmp.ps
 mv X.tmp.pdf $nom_s
 mv $nom_s /Users/francois/Documents/_en_cours/musique/abc/pdf/
