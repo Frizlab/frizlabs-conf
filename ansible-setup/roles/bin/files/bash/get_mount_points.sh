435f7016d96d92be20b2b3f9224b6d696848177c
Frizlab <fload@me.com>
2020-11-25 09:01:20 +0100

    Add get_mount_points

---
+++ ansible-setup/roles/bin/files/bash/get_mount_points.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 image_disk" >/dev/stderr
+	exit 42
+fi
+
+if [ ! -e "$1" ]; then
+	echo "$0: $1: No such file or directory" >/dev/stderr
+	exit 21
+fi
+
+di="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+hdiutil info | awk '
+	BEGIN {
+		FS=": ";
+	}
+
+	/================================================/ {
+		cur_image_path = "";
+		FS=": ";
+	}
+
+	/image-path/ {
+		image_path = $2;
+		FS="\t";
+	}
+
+	/^\/dev\/.*/ {
+		if (image_path == "'"$di"'") {
+			if ($3 != "") print $3;
+		}
+	}
+'
