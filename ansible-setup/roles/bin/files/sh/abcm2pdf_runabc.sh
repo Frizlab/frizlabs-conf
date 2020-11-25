95e77d886bcf2cdb9b6813d92e36fdd3672c92ba
Frizlab <fload@me.com>
2020-11-25 08:21:58 +0100

    Add abcm2pdf_runabc

---
+++ ansible-setup/roles/bin/files/sh/abcm2pdf_runabc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd ~/Library/Preferences/tmp
+
+base=$(basename $1 "")
+
+format=$HOME/Documents/_en_cours/musique/abc/formats/a4_s750.fmt
+nom_s=apprentissage.pdf
+
+abc2.sh -pdf + -abcoptions "-F $format" -noop -- $base
+gv X.tmp.ps
+mv X.tmp.pdf $nom_s
+mv $nom_s /Users/francois/Documents/_en_cours/musique/abc/pdf/
+
+exit
