a1b8f2838afd32406e611d42a5e8f5c8fd0921af
Frizlab <fload@me.com>
2020-12-07 18:23:50 +0100

    Only modify locale file if needed

--- src/components/core/locale.zsh
+++ src/components/core/locale.zsh
@@ -2,11 +2,14 @@
 case "$HOST_OS" in
 	"Darwin") ;; # Nothing to do for Darwin
 	"Linux")
+		readonly TEMP_LOCALE="$(mktemp)"
 		readonly LOCALE_FILE="/etc/locale.gen"
-		readonly SUM="$(shasum "$LOCALE_FILE")"
-		sed -i '/en_US.UTF-8/s/^# //g' "$LOCALE_FILE"
+		sed '/en_US.UTF-8/s/^# //g' "$LOCALE_FILE" >"$TEMP_LOCALE"
 		# We run locale-gen if the locale file has been modified
-		test "$(shasum "$LOCALE_FILE")" = "$SUM" || locale-gen
+		test "$(shasum "$TEMP_LOCALE")" = "$(shasum "$LOCALE_FILE")" || {
+			cat "$TEMP_LOCALE" >"$LOCALE_FILE"
+			locale-gen
+		}
 	;;
 	*)
 		echo_warning "Unknown host OS: $HOST_OS" >/dev/stderr
