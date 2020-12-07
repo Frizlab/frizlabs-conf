ffd02d56ba0866d4d95640a09a225c73318e451b
Frizlab <fload@me.com>
2020-12-07 18:28:48 +0100

    Delete temp locale after using it

--- src/components/core/locale.zsh
+++ src/components/core/locale.zsh
@@ -6,10 +6,11 @@ case "$HOST_OS" in
 		readonly LOCALE_FILE="/etc/locale.gen"
 		sed '/en_US.UTF-8/s/^# //g' "$LOCALE_FILE" >"$TEMP_LOCALE"
 		# We run locale-gen if the locale file has been modified
-		test "$(shasum "$TEMP_LOCALE" | cut -d' ' -f1)" = "$(shasum "$LOCALE_FILE" | cut -d' ' -f1)" || {
+		diff "$TEMP_LOCALE" "$LOCALE_FILE" >/dev/null 2>&1 || {
 			cat "$TEMP_LOCALE" >"$LOCALE_FILE"
 			locale-gen
 		}
+		rm -f "$TEMP_LOCALE"
 	;;
 	*)
 		echo_warning "Unknown host OS: $HOST_OS" >/dev/stderr
