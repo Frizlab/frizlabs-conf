f3c5c08f4c2825de5743f084b5c53d0788fd0f02
Frizlab <fload@me.com>
2020-12-08 15:30:44 +0100

    Set hidden flag on clt folder

--- src/lib/files.zsh
+++ src/lib/files.zsh
@@ -17,9 +17,22 @@ function folder() {
 function acl() {
 	file_name="$1"; acl="$2"
 	
-	test -e "$file_name" || { log_task_failure "cannot set ACL for file at path $folder_name: file not found"; echo "failed"; return }
-	test "$(ls -led "$file_name" | tail -n+2 | sed -E -e 's/^[ \t]*//g' -e 's/[ \t]*$//g')" != "0: $acl" || { echo "ok"; return }
+	test -e "$file_name" || { log_task_failure "cannot set ACL for file at path $file_name: file not found"; echo "failed"; return }
+	test "$(ls -led "$file_name" | tail -n+2 | sed -E -e 's/^[ \t]*//g' -e 's/[ \t]*$//g')" = "0: $acl" && { echo "ok"; return }
 	
-	chmod -E "$file_name" >/dev/null 2>&1 <<<"$acl" || { log_task_failure "cannot set ACL for file at path $folder_name"; echo "failed"; return }
+	chmod -E "$file_name" >/dev/null 2>&1 <<<"$acl" || { log_task_failure "cannot set ACL for file at path $file_name"; echo "failed"; return }
+	echo "changed"
+}
+
+## Make sure the given file or folder has at least the given flag
+## Usage: acl file_or_folder flag
+## Example: acl /var/log "hidden"
+function flags() {
+	file_name="$1"; flag="$2"
+	
+	test -e "$file_name" || { log_task_failure "cannot set flag for file at path $file_name: file not found"; echo "failed"; return }
+	grep -q "$flag" <<<"$(stat -f %Sf "$file_name")" && { echo "ok"; return }
+	
+	chflags "$flag" "$file_name" || { log_task_failure "cannot set flag for file at path $file_name"; echo "failed"; return }
 	echo "changed"
 }
