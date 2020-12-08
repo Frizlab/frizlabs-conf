dea4d3137eeba4c3235253b3ad262b61d2c3ee43
Frizlab <fload@me.com>
2020-12-08 15:35:18 +0100

    Do not set ACL on Linux

--- src/components/core/folders.zsh
+++ src/components/core/folders.zsh
@@ -5,7 +5,7 @@ expected_acl="group:everyone deny delete"
 res=; res_list=()
 CURRENT_TASK_NAME="clt dir"
 {                               res_check "$res" &&   res="$(folder "$CLT_DIR" "700")"             && res_list+=("$res") } || true
-{                               res_check "$res" &&   res="$(acl    "$CLT_DIR" "$expected_acl")"   && res_list+=("$res") } || true
+{ test "$HOST_OS" = "Darwin" && res_check "$res" &&   res="$(acl    "$CLT_DIR" "$expected_acl")"   && res_list+=("$res") } || true
 { test "$HOST_OS" = "Darwin" && res_check "$res" &&   res="$(flags  "$CLT_DIR" "hidden")"          && res_list+=("$res") } || true
 log_task_from_res_list res_list
 
