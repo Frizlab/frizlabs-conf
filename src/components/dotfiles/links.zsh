fb6d5b5a8115218afa7e4cdda533f319b1643994
Frizlab <fload@me.com>
2020-12-09 16:50:47 +0100

    Enhance task names

--- src/components/dotfiles/links.zsh
+++ src/components/dotfiles/links.zsh
@@ -1,7 +1,7 @@
 # Create links to files in this repo, with backup if needed
 
 # First create the backup folder
-CURRENT_TASK_NAME="create backup folder for dotfiles at $DOTFILES_BACKUP_DIR"
+CURRENT_TASK_NAME="dotfiles backup folder $DOTFILES_BACKUP_DIR"
 catchout res_folder folder "$DOTFILES_BACKUP_DIR" "$DOTFILES_BACKUP_DIR_MODE"
 log_task_from_res "$res_folder"
 
@@ -9,7 +9,7 @@ res_check "$res_folder" && {
 	for file in $DOTFILE_FILES; do
 		dest_file="$HOME/$file"
 		repo_file="$(pwd)/files/_$file"
-		CURRENT_TASK_NAME="link $file"
+		CURRENT_TASK_NAME="link $dest_file"
 		catchout res   linknbk "$repo_file" "$dest_file" "600" "$DOTFILES_BACKUP_DIR"
 		log_task_from_res "$res"
 	done
