5fee012d02bf43f8134643ba4ff64d4da0221427
Frizlab <fload@me.com>
2020-12-10 15:22:36 +0100

    Fix compopt path

--- src/components/bin/bins_and_docs.zsh
+++ src/components/bin/bins_and_docs.zsh
@@ -138,15 +138,20 @@ doc "frizlab" ":Darwin:" "man/man1" "docs/make-link.1"
 delete_bin "frizlab" "my_split"
 bin "frizlab" ":Darwin:" "bin/my-split"
 
+
 ### compopt ###
 # When compopt is not available (e.g. w/ Bash 3), this hides the error we get
 # from some completion scripts that use compopt.
 res=; res_list=()
-src=; # Computed later
 dest="$THIRD_PARTY_BIN_DIR/compopt"
 backup_dir="$THIRD_PARTY_BIN_DIR/$BIN_BACKUP_DIR_BASENAME"
 CURRENT_TASK_NAME="link fake compopt -> ${dest/#$HOME/\~}"
-{ res_check "$res" &&   src="$(command -v true)" || { log_task_failure "cannot get path of bin “true”."; res_list+=("failed") } }
-{ res_check "$res" &&   catchout res  folder "$backup_dir" "$BIN_BACKUP_DIR_MODE"                     && res_list+=("$res") }
-{ res_check "$res" &&   catchout res  linknbk "/usr/bin/true" "$dest" "755" "$backup_dir"             && res_list+=("$res") }
+src=
+test -x "$src" || src="/usr/bin/true"                  || true
+test -x "$src" || src="/bin/true"                      || true
+test -x "$src" || src="$(command -v true 2>/dev/null)" || true
+test -x "$src" || src="$(which true 2>/dev/null)"      || true
+test -x "$src" || { log_task_failure "cannot get path of bin “true”."; res="failed"; res_list+=("$res") }
+{ res_check "$res" &&   catchout res  folder "$backup_dir" "$BIN_BACKUP_DIR_MODE" && res_list+=("$res") }
+{ res_check "$res" &&   catchout res  linknbk "$src" "$dest" "755" "$backup_dir"  && res_list+=("$res") }
 log_task_from_res_list res_list
