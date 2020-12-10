4d53ed6a33386b51786365e05416ac6c97f0900b
Frizlab <fload@me.com>
2020-12-10 19:23:32 +0100

    Skip Python 3.8 and 3.7 installation
    Installation of these two packages fails for now.

--- src/components/package-managers/main.zsh
+++ src/components/package-managers/main.zsh
@@ -11,8 +11,9 @@ log_task_from_res "$res"
 CURRENT_TASK_NAME="python3 homebrew";   catchout res  install_homebrew "$HOMEBREW_PYTHON3_DIR";                                                log_task_from_res "$res"
 CURRENT_TASK_NAME="install python@3";   catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3"    "opt/python@3/bin/python3";   log_task_from_res "$res"
 CURRENT_TASK_NAME="install python@3.9"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.9"  "opt/python@3.9/bin/python3"; log_task_from_res "$res"
-CURRENT_TASK_NAME="install python@3.8"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.8"  "opt/python@3.8/bin/python3"; log_task_from_res "$res"
-CURRENT_TASK_NAME="install python@3.7"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.7"  "opt/python@3.7/bin/python3"; log_task_from_res "$res"
+# We do not install Python 3.8 and 3.7 because the compilation fails atm
+#CURRENT_TASK_NAME="install python@3.8"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.8"  "opt/python@3.8/bin/python3"; log_task_from_res "$res"
+#CURRENT_TASK_NAME="install python@3.7"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.7"  "opt/python@3.7/bin/python3"; log_task_from_res "$res"
 
 # And finally a Homebrew install for Python 2!
 #CURRENT_TASK_NAME="python2 homebrew"
