28eafa7edae096240d2364af97972579e7dd9a7a
Frizlab <fload@me.com>
2020-12-10 18:59:25 +0100

    Install python3 Homebrew

--- src/components/package-managers/main.zsh
+++ src/components/package-managers/main.zsh
@@ -8,13 +8,11 @@ catchout res  install_homebrew "$HOMEBREW_DIR"
 log_task_from_res "$res"
 
 # Then a Homebrew install for Python 3.9
-CURRENT_TASK_NAME="python3 homebrew"
-{ res_check "$res" &&   catchout res  install_homebrew "$HOMEBREW_PYTHON3_DIR"   && res_list+=("$res") }
-{ res_check "$res" &&   catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3"    "opt/python@3/bin/python3"     && res_list+=("$res") }
-{ res_check "$res" &&   catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.9"  "opt/python@3.9/bin/python3"   && res_list+=("$res") }
-{ res_check "$res" &&   catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.8"  "opt/python@3.8/bin/python3"   && res_list+=("$res") }
-{ res_check "$res" &&   catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.7"  "opt/python@3.7/bin/python3"   && res_list+=("$res") }
-log_task_from_res_list res_list
+CURRENT_TASK_NAME="python3 homebrew";   catchout res  install_homebrew "$HOMEBREW_DIR";                                                        log_task_from_res "$res"
+CURRENT_TASK_NAME="install python@3";   catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3"    "opt/python@3/bin/python3";   log_task_from_res "$res"
+CURRENT_TASK_NAME="install python@3.9"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.9"  "opt/python@3.9/bin/python3"; log_task_from_res "$res"
+CURRENT_TASK_NAME="install python@3.8"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.8"  "opt/python@3.8/bin/python3"; log_task_from_res "$res"
+CURRENT_TASK_NAME="install python@3.7"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.7"  "opt/python@3.7/bin/python3"; log_task_from_res "$res"
 
 # And finally a Homebrew install for Python 2!
 #CURRENT_TASK_NAME="python2 homebrew"
