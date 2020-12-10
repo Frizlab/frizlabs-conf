89ad03ad327fc8e05746ea00bb1ecee94671fd2e
Frizlab <fload@me.com>
2020-12-10 18:17:50 +0100

    Fix case where setting a defaults did not work if the value was never set for the given key

--- src/lib/defaults.zsh
+++ src/lib/defaults.zsh
@@ -9,7 +9,9 @@ function defaults_set_int() {
 	value="$3"
 	
 	# Note: We do not handle the case where the key does not have the correct type
-	current_value="$(defaults read "$domain" "$key" 2>/dev/null)" || { log_task_failure "cannot get current value for defaults domain $domain key $key"; echo "failed"; return }
+	# We fully ignore if defaults cannot read the default at all, because it will
+	# probably be because the key does not exist and it is a normal error.
+	current_value="$(defaults read "$domain" "$key" 2>/dev/null)" || true
 	test "$current_value" != "$value" || { echo "ok"; return }
 	
 	defaults write "$domain" "$key" -int "$value" >/dev/null 2>&1 || { log_task_failure "cannot set value for defaults domain $domain key $key"; echo "failed"; return }
