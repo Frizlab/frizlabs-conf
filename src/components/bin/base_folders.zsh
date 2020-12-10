f681e68846976a8ff5397fd63913fe95c166f657
Frizlab <fload@me.com>
2020-12-10 16:16:00 +0100

    Alignment fix

--- src/components/bin/base_folders.zsh
+++ src/components/bin/base_folders.zsh
@@ -2,18 +2,18 @@
 
 
 CURRENT_TASK_NAME="folder ${FIRST_PARTY_BIN_DIR/#$HOME/\~}"
-catchout res   folder "$FIRST_PARTY_BIN_DIR" "755"
+catchout res  folder "$FIRST_PARTY_BIN_DIR" "755"
 log_task_from_res "$res"
 
 CURRENT_TASK_NAME="folder ${THIRD_PARTY_BIN_DIR/#$HOME/\~}"
-catchout res   folder "$THIRD_PARTY_BIN_DIR" "755"
+catchout res  folder "$THIRD_PARTY_BIN_DIR" "755"
 log_task_from_res "$res"
 
 
 CURRENT_TASK_NAME="folder ${FIRST_PARTY_SHARE_DIR/#$HOME/\~}"
-catchout res   folder "$FIRST_PARTY_SHARE_DIR" "755"
+catchout res  folder "$FIRST_PARTY_SHARE_DIR" "755"
 log_task_from_res "$res"
 
 CURRENT_TASK_NAME="folder ${THIRD_PARTY_SHARE_DIR/#$HOME/\~}"
-catchout res   folder "$THIRD_PARTY_SHARE_DIR" "755"
+catchout res  folder "$THIRD_PARTY_SHARE_DIR" "755"
 log_task_from_res "$res"
