c03773f91e4d02688ca87754efb9754e0d3f8801
Frizlab <fload@me.com>
2020-12-10 19:06:49 +0100

    Fix python3 homebrew install path

--- src/components/package-managers/main.zsh
+++ src/components/package-managers/main.zsh
@@ -8,7 +8,7 @@ catchout res  install_homebrew "$HOMEBREW_DIR"
 log_task_from_res "$res"
 
 # Then a Homebrew install for Python 3.9
-CURRENT_TASK_NAME="python3 homebrew";   catchout res  install_homebrew "$HOMEBREW_DIR";                                                        log_task_from_res "$res"
+CURRENT_TASK_NAME="python3 homebrew";   catchout res  install_homebrew "$HOMEBREW_PYTHON3_DIR";                                                log_task_from_res "$res"
 CURRENT_TASK_NAME="install python@3";   catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3"    "opt/python@3/bin/python3";   log_task_from_res "$res"
 CURRENT_TASK_NAME="install python@3.9"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.9"  "opt/python@3.9/bin/python3"; log_task_from_res "$res"
 CURRENT_TASK_NAME="install python@3.8"; catchout res  install_brew_package "$HOMEBREW_PYTHON3_DIR" "python@3.8"  "opt/python@3.8/bin/python3"; log_task_from_res "$res"
