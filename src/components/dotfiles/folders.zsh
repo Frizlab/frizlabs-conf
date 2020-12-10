21624ed7f33199693f9fb76fe731994c477b8d3d
Frizlab <fload@me.com>
2020-12-10 08:33:13 +0100

    Enhance some logs

--- src/components/dotfiles/folders.zsh
+++ src/components/dotfiles/folders.zsh
@@ -1,6 +1,6 @@
 # Create the folders the different dotfiles expect (extension points)
 for folder in $DOTFILE_FOLDERS; do
-	CURRENT_TASK_NAME="folder $HOME/$folder"
+	CURRENT_TASK_NAME="folder ~/$folder"
 	catchout res   folder "$HOME/$folder" "700"
 	log_task_from_res "$res"
 done
