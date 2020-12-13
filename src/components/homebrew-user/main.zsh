70a6e12005a09d60ef8157065c4f23513ddd292f
Frizlab <fload@me.com>
2020-12-13 18:24:53 +0100

    Install x86 base formulae

--- src/components/homebrew-user/main.zsh
+++ src/components/homebrew-user/main.zsh
@@ -16,20 +16,3 @@ case "$HOST_OS:$HOST_ARCH" in
 		CURRENT_TASK_NAME="install user homebrew (unknown arch)";   catchout res  install_homebrew "$HOMEBREW_NATIVE_USER_DIR";      log_task_from_res "$res"
 	;;
 esac
-
-#
-## First a normal Homebrew install, in $HOMEBREW_DIR, w/ its formulae and casks.
-#CURRENT_TASK_NAME="install main homebrew"; catchout res  install_homebrew "$HOMEBREW_DIR"; log_task_from_res "$res"
-#
-## We install the x86-only formulae in main Homebrew if not on Darwin arm64,
-## otherwise we install a separate Homebrew dedicated for x86 installs
-#if test "$HOST_OS:$HOST_ARCH" != "Darwin:arm64"; then
-#	for formula_name path_to_check in ${(kv)X86_HOMEBREW_FORMULAE}; do
-#		CURRENT_TASK_NAME="install formula $formula_name";   catchout res  install_brew_package "$HOMEBREW_DIR" "$formula_name" "$path_to_check" "--formula";   log_task_from_res "$res"
-#	done
-#else
-#	CURRENT_TASK_NAME="install homebrew x86"; catchout res  install_homebrew "$HOMEBREW_X86_DIR"; log_task_from_res "$res"
-#	for formula_name path_to_check in ${(kv)X86_HOMEBREW_FORMULAE}; do
-#		CURRENT_TASK_NAME="install formula $formula_name";   catchout res  install_brew_package "--force-arch" "x86_64" "$HOMEBREW_X86_DIR" "$formula_name" "$path_to_check" "--formula";   log_task_from_res "$res"
-#	done
-#fi
--- src/components/pkgs-brew-system-base/main.zsh
+++ src/components/pkgs-brew-system-base/main.zsh
@@ -3,3 +3,15 @@
 for formula_name path_to_check in ${(kv)MAIN_SYSTEM_HOMEBREW_FORMULAE}; do
 	CURRENT_TASK_NAME="install formula $formula_name";   catchout res  install_brew_package "$HOMEBREW_SYSTEM_DIR" "$formula_name" "$path_to_check" "--formula";   log_task_from_res "$res"
 done
+
+# We install the x86-only formulae in main Homebrew if not on Darwin arm64,
+# otherwise we install a separate Homebrew dedicated for x86 installs
+if test "$HOST_OS:$HOST_ARCH" != "Darwin:arm64"; then
+	for formula_name path_to_check in ${(kv)X86_SYSTEM_HOMEBREW_FORMULAE}; do
+		CURRENT_TASK_NAME="install formula $formula_name";   catchout res  install_brew_package                         "$HOMEBREW_SYSTEM_DIR"     "$formula_name" "$path_to_check" "--formula";   log_task_from_res "$res"
+	done
+else
+	for formula_name path_to_check in ${(kv)X86_SYSTEM_HOMEBREW_FORMULAE}; do
+		CURRENT_TASK_NAME="install formula $formula_name";   catchout res  install_brew_package "--force-arch" "x86_64" "$HOMEBREW_X86_SYSTEM_DIR" "$formula_name" "$path_to_check" "--formula";   log_task_from_res "$res"
+	done
+fi
