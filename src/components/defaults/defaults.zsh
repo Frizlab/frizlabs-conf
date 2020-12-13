3672bce24fec73f11b8e9e2a90d8bdc5acac4639
Frizlab <fload@me.com>
2020-12-13 17:28:55 +0100

    Set defaults to not close windows when quitting app

--- src/components/defaults/defaults.zsh
+++ src/components/defaults/defaults.zsh
@@ -7,3 +7,7 @@ log_task_from_res "$res"
 CURRENT_TASK_NAME="set initial key repeat"
 catchout res  defaults_set_int NSGlobalDomain InitialKeyRepeat 15
 log_task_from_res "$res"
+
+CURRENT_TASK_NAME="do not close window when app quit"
+catchout res  defaults_set_bool NSGlobalDomain NSQuitAlwaysKeepsWindows 1
+log_task_from_res "$res"
--- src/lib/defaults.zsh
+++ src/lib/defaults.zsh
@@ -4,9 +4,9 @@
 ## Usage: defaults_set_int domain key value
 ## Example: defaults_set_int NSGlobalDomain KeyRepeat 2
 function defaults_set_int() {
-	domain="$1"
-	key="$2"
-	value="$3"
+	local domain="$1"
+	local key="$2"
+	local value="$3"
 	
 	# Note: We do not handle the case where the key does not have the correct type
 	# We fully ignore if defaults cannot read the default at all, because it will
@@ -15,5 +15,27 @@ function defaults_set_int() {
 	test "$current_value" != "$value" || { echo "ok"; return }
 	
 	defaults write "$domain" "$key" -int "$value" >/dev/null 2>&1 || { log_task_failure "cannot set value for defaults domain $domain key $key"; echo "failed"; return }
+	echo "changed"
+}
+
+## Set a bool key for the given key using macOS defaults CLT.
+## For the value, anything other than 0 will be considered to be true.
+## Usage: defaults_set_bool domain key value
+## Example: defaults_set_bool NSGlobalDomain NSQuitAlwaysKeepsWindows 1
+function defaults_set_bool() {
+	local domain="$1"
+	local key="$2"
+	local value="$3"
+	
+	if test "$value" = "0"; then expected_value="0"; value_to_set="no";
+	else                         expected_value="1"; value_to_set="yes"; fi
+	
+	# Note: We do not handle the case where the key does not have the correct type
+	# We fully ignore if defaults cannot read the default at all, because it will
+	# probably be because the key does not exist and it is a normal error.
+	current_value="$(defaults read "$domain" "$key" 2>/dev/null)" || true
+	test "$current_value" != "$expected_value" || { echo "ok"; return }
+	
+	defaults write "$domain" "$key" -bool "$value_to_set" >/dev/null 2>&1 || { log_task_failure "cannot set value for defaults domain $domain key $key"; echo "failed"; return }
 	echo "changed"
 }
