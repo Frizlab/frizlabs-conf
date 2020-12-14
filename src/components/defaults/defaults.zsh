5145fecb4b2ab08f850e2ebc1f2eb9cefa1d19cb
Frizlab <fload@me.com>
2020-12-14 11:58:12 +0100

    Add more defaults

--- src/components/defaults/defaults.zsh
+++ src/components/defaults/defaults.zsh
@@ -11,3 +11,15 @@ log_task_from_res "$res"
 CURRENT_TASK_NAME="do not close window when app quit"
 catchout res  defaults_set_bool NSGlobalDomain NSQuitAlwaysKeepsWindows 1
 log_task_from_res "$res"
+
+CURRENT_TASK_NAME="do not automatically rearrange spaces based on most recent use"
+catchout res  defaults_set_bool com.apple.dock mru-spaces 0
+log_task_from_res "$res"
+
+CURRENT_TASK_NAME="group windows by app in expose"
+catchout res  defaults_set_bool com.apple.dock expose-group-apps 1
+log_task_from_res "$res"
+
+CURRENT_TASK_NAME="do not switch to space w/ open window for app when app activates"
+catchout res  defaults_set_bool NSGlobalDomain AppleSpacesSwitchOnActivate 0
+log_task_from_res "$res"
