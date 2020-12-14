dcad3686c5c6089806f3b1f6163f820b3a60d3eb
Frizlab <fload@me.com>
2020-12-14 16:15:54 +0100

    Add active corners definition

--- src/components/defaults/defaults.zsh
+++ src/components/defaults/defaults.zsh
@@ -23,3 +23,27 @@ log_task_from_res "$res"
 CURRENT_TASK_NAME="do not switch to space w/ open window for app when app activates"
 catchout res  defaults_set_bool NSGlobalDomain AppleSpacesSwitchOnActivate 0
 log_task_from_res "$res"
+
+res=; res_list=()
+CURRENT_TASK_NAME="set top-left corner action"
+{ res_check "$res" &&   catchout res  defaults_set_int com.apple.dock wvous-tl-corner   2 && res_list+=("$res") }
+{ res_check "$res" &&   catchout res  defaults_set_int com.apple.dock wvous-tl-modifier 0 && res_list+=("$res") }
+log_task_from_res_list res_list
+
+res=; res_list=()
+CURRENT_TASK_NAME="set top-right corner action"
+{ res_check "$res" &&   catchout res  defaults_set_int com.apple.dock wvous-tr-corner   12 && res_list+=("$res") }
+{ res_check "$res" &&   catchout res  defaults_set_int com.apple.dock wvous-tr-modifier  0 && res_list+=("$res") }
+log_task_from_res_list res_list
+
+res=; res_list=()
+CURRENT_TASK_NAME="set bottom-right corner action"
+{ res_check "$res" &&   catchout res  defaults_set_int com.apple.dock wvous-br-corner   6 && res_list+=("$res") }
+{ res_check "$res" &&   catchout res  defaults_set_int com.apple.dock wvous-br-modifier 0 && res_list+=("$res") }
+log_task_from_res_list res_list
+
+res=; res_list=()
+CURRENT_TASK_NAME="set bottom-left corner action"
+{ res_check "$res" &&   catchout res  defaults_set_int com.apple.dock wvous-bl-corner   5 && res_list+=("$res") }
+{ res_check "$res" &&   catchout res  defaults_set_int com.apple.dock wvous-bl-modifier 0 && res_list+=("$res") }
+log_task_from_res_list res_list
