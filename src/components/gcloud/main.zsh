f75385ebd30f062d7b66218c94a423481789f9a8
Frizlab <fload@me.com>
2021-02-28 19:32:55 +0100

    Fix arch check in gcloud install on Linux

--- src/components/gcloud/main.zsh
+++ src/components/gcloud/main.zsh
@@ -6,7 +6,7 @@ case "$HOST_OS:$HOST_ARCH" in
 		# Note: We ignore 32-bits macOS (it does not exist anymore)
 		tar_url="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-320.0.0-darwin-x86_64.tar.gz"
 	;;
-	Linux:x86)
+	Linux:x86_64)
 		tar_url="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-320.0.0-linux-x86_64.tar.gz"
 	;;
 esac
