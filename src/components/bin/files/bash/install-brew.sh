5db551a2ee6cb964575a22580d198c1b9cd5687b
Frizlab <fload@me.com>
2021-02-28 10:50:44 +0100

    Remove a check in install-brew command

--- src/components/bin/files/bash/install-brew.sh
+++ src/components/bin/files/bash/install-brew.sh
@@ -22,15 +22,18 @@ readonly LOCAL_HOMEBREW_INSTALL="$1"
 readonly BREW="${LOCAL_HOMEBREW_INSTALL}/bin/brew"
 
 if [ ! -x "$BREW" ]; then
-	if [ ! -d "$(dirname "$LOCAL_HOMEBREW_INSTALL")" ]; then
-		echo "Asked to install brew at path '$LOCAL_HOMEBREW_INSTALL' but parent folder does not exist." >&2
-		exit 4
-	fi
-	if [ -e "$LOCAL_HOMEBREW_INSTALL" -a "$(ls $LOCAL_HOMEBREW_INSTALL | wc -l)" -gt 0 ]; then
+	# I kinda like this check, because it makes sure parent folders are
+	# consciously created w/ correct perms before installing brew. However it is
+	# annoying in some cases, so we disable it, at least for now.
+#	if [ ! -d "$(dirname -- "$LOCAL_HOMEBREW_INSTALL")" ]; then
+#		echo "Asked to install brew at path '$LOCAL_HOMEBREW_INSTALL' but parent folder does not exist." >&2
+#		exit 4
+#	fi
+	if [ -e "$LOCAL_HOMEBREW_INSTALL" -a "$(ls -- "$LOCAL_HOMEBREW_INSTALL" | wc -l)" -gt 0 ]; then
 		echo "brew binary does not exist at path '$BREW', but '$LOCAL_HOMEBREW_INSTALL' exists." >&2
 		exit 3
 	fi
-	mkdir -p "$LOCAL_HOMEBREW_INSTALL"
+	mkdir -p -- "$LOCAL_HOMEBREW_INSTALL"
 	curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "${LOCAL_HOMEBREW_INSTALL}"
 	HOMEBREW_NO_ANALYTICS=1 "$BREW" update
 fi
