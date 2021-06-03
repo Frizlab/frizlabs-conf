6c9158d21a8faebd99fc77f561088d5ba67d34ab
Frizlab <fload@me.com>
2021-06-03 11:02:23 +0200

    Add ls-diff in home brew package

--- src/vars-home.zsh
+++ src/vars-home.zsh
@@ -5,3 +5,11 @@ readonly DEFAULT_BREW_IS_SYSTEM=false
 MAIN_USER_HOMEBREW_FORMULAE+=(
 	"abcm2ps" "bin/abcm2ps"
 )
+
+if test "$HOST_OS" != "Linux"; then
+	# These formulae do not compile on Linux (at the time of writing), so we only
+	# add them when we’re not on Linux.
+	MAIN_USER_HOMEBREW_FORMULAE+=(
+		"frizlab/perso/ls-diff" "bin/ls-diff"
+	)
+fi
