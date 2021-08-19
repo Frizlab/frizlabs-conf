26625aab54177b05a6d5a7388b3b8572a88d8cc3
Frizlab <fload@me.com>
2021-08-19 17:45:00 +0200

    Check for launchctl before doing launchd component

--- src/components/launchd/main.zsh
+++ src/components/launchd/main.zsh
@@ -1,14 +1,8 @@
-# Launch Agents vs Launch Daemons: https://apple.stackexchange.com/a/290946
-
-source "./lib/launch-agents.zsh"
-
-# TODO: Do not execute an sh script. It poses two problems:
-#    - First, sh is not allowed to read in ~/Documents, so we have to copy the
-#      script someplace else. But that’s workarounded, we have created
-#      LAUNCHD_CLT_BIN_DIR exactly for this;
-#    - Secondly, and maybe more importantly, for this script to run, the
-#      executable running it must have access to Apple Events for Safari and
-#      System Events. And thus we have to give access to /bin/sh to these events
-#      which might be dangerous (if we’re paranoid).
-# TODO: Correctly manage output (currently each run is appended to output file).
-install_user_launch_agent "me.frizlab.dump-safari-tabs"
+# Nothing to be done if the launchctl command is not available. We still print
+# ok in a dummy task to have a more beautiful output.
+if ! command -v launchctl >/dev/null 2>&1; then
+	start_task "launchctl is not available (probably not on macOS); skipping this component…"
+	log_task_from_res "ok"
+else
+	source "./launchd.zsh"
+fi
