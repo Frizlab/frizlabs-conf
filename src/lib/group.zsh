1ba64df6dbd61f15860065071ca6911365be2a04
Frizlab <fload@me.com>
2021-11-16 11:04:57 +0100

    Fix multiple bugs in computer group selection

--- src/lib/group.zsh
+++ src/lib/group.zsh
@@ -3,18 +3,22 @@ readonly COMPUTER_GROUP_PATH="$CACHE_FOLDER/computer_group"
 declare -rA COMPUTER_GROUPS=(
 	1 home
 	2 work
-	2 vm
+	3 vm
 )
 
 test -f "$COMPUTER_GROUP_PATH" || {
 	message=
 	message+=$'What group is your computer in?\n\n'
 	message+=$'Choose a number (group will be "unknown" if any other value is given):\n'
-	for k v in ${(kv)COMPUTER_GROUPS}; do
-		message+="   $k) $v"$'\n'
+	for k in ${(kon)COMPUTER_GROUPS}; do
+		message+="   $k) $COMPUTER_GROUPS[$k]"$'\n'
 	done
 	message+='Your choice: '
 	read -r "group?$message"
-	echo -n "${COMPUTER_GROUPS[$group]}" >"$COMPUTER_GROUP_PATH"
+	if [ "$group" -gt 0 -a "$group" -le "${#COMPUTER_GROUPS[@]}" ] 2>/dev/null; then
+		echo -n "${COMPUTER_GROUPS[$group]}" >"$COMPUTER_GROUP_PATH"
+	else
+		echo -n "unknown" >"$COMPUTER_GROUP_PATH"
+	fi
 }
 COMPUTER_GROUP="$(cat "$COMPUTER_GROUP_PATH")"; readonly COMPUTER_GROUP
