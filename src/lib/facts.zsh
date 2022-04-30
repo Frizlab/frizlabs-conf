17bf09e103b3b642bd968f5133596bbaa86e871b
Frizlab <fload@me.com>
2022-04-30 13:21:29 +0200

    Do not fail facts if tput fails

--- src/lib/facts.zsh
+++ src/lib/facts.zsh
@@ -3,8 +3,9 @@
 HOST_OS="$(uname -s)";   readonly HOST_OS
 HOST_ARCH="$(uname -m)"; readonly HOST_ARCH
 
-USER_ID=$(id -u); readonly CURRENT_USER_ID
-
 # Note: “typeset -r” is strictly equivalent (AFAICT) to “readonly”.
 # We kept one “typeset -r” as a kind of documentation for the future.
-TERM_WIDTH="$(tput cols)"; typeset -r TERM_WIDTH
+USER_ID=$(id -u); typeset -r CURRENT_USER_ID
+
+# tput can fail if there are no tty (e.g. in a docker build context), hence the “|| print 0”.
+TERM_WIDTH="$(tput cols 2>/dev/null || print 0)"; readonly TERM_WIDTH
