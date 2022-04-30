4eeb6b9bf81c82d7f99ef2bfbb006c40cd0188a4
Frizlab <fload@me.com>
2022-04-30 21:58:50 +0200

    Migrate gcloud to the new conventions

--- src/components/gcloud/main.zsh
+++ src/components/gcloud/main.zsh
@@ -22,10 +22,10 @@ esac
 start_task "install gcloud"
 if test -n "$tar_url"; then
 	RES=; RES_LIST=()
-	{ res_check "$RES" &&   temp="$(mktemp)"   || { log_task_failure "cannot create temporary file"   && RES_LIST+=("failed") } }
-	{ res_check "$RES" &&   catchout RES  folder "$GCLOUD_DIR" "755"                                  && RES_LIST+=("$RES") }
-	{ res_check "$RES" &&   test -x "$GCLOUD_DIR/bin/gcloud"                                          && RES_LIST+=("ok") } || {
-		{ res_check "$RES" &&   curl -L "$tar_url" 2>/dev/null | "$TAR" xz --strip 1 -C "$GCLOUD_DIR" && RES_LIST+=("$RES") }
+	{ res_check "$RES" &&   temp="$(run_and_log_keep_stdout mktemp)"   || { log_task_failure "cannot create temporary file" && RES_LIST+=("failed") } }
+	{ res_check "$RES" &&   catchout RES  libfiles__folder "$GCLOUD_DIR" "755"                                              && RES_LIST+=("$RES") }
+	{ res_check "$RES" &&   run_and_log test -x "$GCLOUD_DIR/bin/gcloud"                                                    && RES_LIST+=("ok") } || {
+		{ res_check "$RES" &&   run_and_log_keep_stdout curl -L "$tar_url" | run_and_log "$TAR" xz --strip 1 -C "$GCLOUD_DIR" && RES_LIST+=("ok") || { log_task_failure "cannot download or extract gcloud" && RES_LIST+=("failed") } }
 	}
 	log_task_from_res_list RES_LIST
 	
@@ -35,10 +35,10 @@ if test -n "$tar_url"; then
 	log_task_from_res "$RES"
 	
 	start_task "install zsh completion"
-	catchout RES  lnk "$GCLOUD_DIR/completion.zsh.inc"  "$HOME/.zshrc.d/590-gougle-cloud-sdk-completion.sh"  "600"
+	catchout RES  libfiles__lnk "$GCLOUD_DIR/completion.zsh.inc"  "$HOME/.zshrc.d/590-gougle-cloud-sdk-completion.sh"  "600"
 	log_task_from_res "$RES"
 	start_task "install bash completion"
-	catchout RES  lnk "$GCLOUD_DIR/completion.bash.inc" "$HOME/.bashrc.d/590-gougle-cloud-sdk-completion.sh" "600"
+	catchout RES  libfiles__lnk "$GCLOUD_DIR/completion.bash.inc" "$HOME/.bashrc.d/590-gougle-cloud-sdk-completion.sh" "600"
 	log_task_from_res "$RES"
 else
 	log_task_failure "cannot install gcloud (unable to infer gcloud URL from platform/arch)"
