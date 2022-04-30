ee729ebe421ae99951ab4f5fadaab8762770515a
Frizlab <fload@me.com>
2022-04-30 12:35:24 +0200

    Add sbin folders in clt

--- src/components/bin/base_folders.zsh
+++ src/components/bin/base_folders.zsh
@@ -3,6 +3,9 @@
 task__folder "$FIRST_PARTY_BIN_DIR" "755"
 task__folder "$THIRD_PARTY_BIN_DIR" "755"
 
+task__folder "$FIRST_PARTY_SBIN_DIR" "755"
+task__folder "$THIRD_PARTY_SBIN_DIR" "755"
+
 task__folder "$FIRST_PARTY_SHARE_DIR" "755"
 task__folder "$THIRD_PARTY_SHARE_DIR" "755"
 
--- src/vars.zsh
+++ src/vars.zsh
@@ -8,6 +8,7 @@ readonly CLT_DIR="$HOME/clt"
 # All binaries/scripts whose author is the same as the current user will go there.
 readonly FIRST_PARTY_CLT_DIR="$CLT_DIR/1st-party-ad-hoc"
 readonly FIRST_PARTY_BIN_DIR="$FIRST_PARTY_CLT_DIR/bin"
+readonly FIRST_PARTY_SBIN_DIR="$FIRST_PARTY_CLT_DIR/sbin"
 readonly FIRST_PARTY_SHARE_DIR="$FIRST_PARTY_CLT_DIR/share"
 readonly FIRST_PARTY_BASH_COMPLETIONS="$FIRST_PARTY_SHARE_DIR/bash/completions"
 readonly FIRST_PARTY_ZSH_SITE_FUNCTIONS="$FIRST_PARTY_SHARE_DIR/zsh/site-functions"
@@ -15,6 +16,7 @@ readonly FIRST_PARTY_ZSH_SITE_FUNCTIONS="$FIRST_PARTY_SHARE_DIR/zsh/site-functio
 # Other binaries and scripts.
 readonly THIRD_PARTY_CLT_DIR="$CLT_DIR/3rd-party-ad-hoc"
 readonly THIRD_PARTY_BIN_DIR="$THIRD_PARTY_CLT_DIR/bin"
+readonly THIRD_PARTY_SBIN_DIR="$THIRD_PARTY_CLT_DIR/sbin"
 readonly THIRD_PARTY_SHARE_DIR="$THIRD_PARTY_CLT_DIR/share"
 readonly THIRD_PARTY_BASH_COMPLETIONS="$THIRD_PARTY_SHARE_DIR/bash/completions"
 readonly THIRD_PARTY_ZSH_SITE_FUNCTIONS="$THIRD_PARTY_SHARE_DIR/zsh/site-functions"
