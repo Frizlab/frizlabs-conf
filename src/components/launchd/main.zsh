88f498acb375400a3de9e014abe3bd40ab6b3750
Frizlab <fload@me.com>
2022-04-30 21:38:12 +0200

    Enhance an error message

--- src/components/launchd/main.zsh
+++ src/components/launchd/main.zsh
@@ -1,7 +1,7 @@
 # Nothing to be done if the launchctl command is not available.
 # We still print ok in a dummy task to have a more beautiful output.
 if ! command -v launchctl >/dev/null 2>&1; then
-	start_task "launchctl is not available (probably not on macOS); skipping this component…"
+	start_task "launchctl is not available (we’re probably not on macOS); skipping this component…"
 	log_task_from_res "ok"
 else
 	source "./launchd.zsh"
