75af00891eb2af3de160f0e97619c0faa7f45779
Frizlab <fload@me.com>
2022-04-30 21:32:07 +0200

    Use gem directly in gem-list-leaves

--- src/components/bin/files/sh/gem-list-leaves.sh
+++ src/components/bin/files/sh/gem-list-leaves.sh
@@ -2,12 +2,11 @@
 # List Ruby gems, that are not a dependency of any other gem (analog of `brew leaves`)
 # https://gist.github.com/astyagun/290b783045afffb8190a0c75ab76d0fa
 
-GEM="${HOME}/usr/homebrew/bin/gem"
 
 GEMS_FILE=`mktemp`
 DEPENDENCIES_FILE=`mktemp`
 
-"$GEM" list -l | sed 's/ (.*//' | sort >"$GEMS_FILE"
-cat "$GEMS_FILE" | xargs -n1 "$GEM" dependency -l --pipe | sed 's/ --version.*//' | sort -u >"$DEPENDENCIES_FILE"
+gem list -l | sed 's/ (.*//' | sort >"$GEMS_FILE"
+cat "$GEMS_FILE" | xargs -n1 gem dependency -l --pipe | sed 's/ --version.*//' | sort -u >"$DEPENDENCIES_FILE"
 comm -23 "$GEMS_FILE" "$DEPENDENCIES_FILE"
 rm -f "$GEMS_FILE $DEPENDENCIES_FILE"
