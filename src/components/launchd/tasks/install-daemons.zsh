c2c9f13beca705b92b51d5145d077624f9df2d17
Frizlab <fload@me.com>
2022-05-03 22:45:43 +0200

    Log output of dump-safari-tabs to clt logs dir

--- src/components/launchd/tasks/install-daemons.zsh
+++ src/components/launchd/tasks/install-daemons.zsh
@@ -1,6 +1,8 @@
+source "./vars.zsh"
+
 source "./lib/launch-agents.zsh"
 
-# Launch Agents vs Launch Daemons: https://apple.stackexchange.com/a/290946
-
+# Install dump-safari-tabs agent.
 # TODO: Correctly manage output (currently each run is appended to output file).
+task__folder "$LAUNCHD__DUMP_SAFARI_TABS_LOGS_DIR" "755"
 launchd_task__install_user_launch_agent "me.frizlab.dump-safari-tabs"
---
+++ src/components/launchd/vars.zsh
@@ -0,0 +1,1 @@
+readonly LAUNCHD__DUMP_SAFARI_TABS_LOGS_DIR="$CLT_LOGS_DIR/dump-safari-tabs"
