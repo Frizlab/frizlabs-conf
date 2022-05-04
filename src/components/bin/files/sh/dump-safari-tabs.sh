0ab28a150f6a01f943938653909c17f09324dd71
Frizlab <fload@me.com>
2022-05-04 23:10:17 +0200

    Make dump-safari-tabs support more than one browser to dump at a time

--- src/components/bin/files/sh/dump-safari-tabs.sh
+++ src/components/bin/files/sh/dump-safari-tabs.sh
@@ -2,7 +2,10 @@
 set -eu
 
 
-# Tests were done on 2022-05-03
+# Usage: dump-safari-tabs [browser ...]
+#
+# Default browser is Safari.
+#
 # This script is compatible with:
 #   - Safari
 #   - Safari Technology Preview
@@ -11,7 +14,8 @@ set -eu
 # It is _not_ compatible with:
 #   - DuckDuckGo
 #   - Firefox
-readonly BROWSER="${1:-Safari}"
+#
+# Tests were done on 2022-05-03
 
 
 # osascript apparently logs to stderr, so we redirect stderr to stdout and
@@ -26,41 +30,46 @@ function redirect_osascript_output() {
 }
 
 
-# Call AppleScript.
-osascript -e '
--- Get the list of running processes
-tell application "System Events"
-	set listOfRunningProcesses to (name of every process)
-end tell
-
--- Check the selected browser is currently running
-if (listOfRunningProcesses contains "'"$BROWSER"'") then
-	tell application "'"$BROWSER"'"
-		repeat with theWindow in windows
-			-- For the bounds definition: https://www.macosxautomation.com/applescript/firsttutorial/11.html
-			set b to bounds of theWindow
-			-- The distance in pixels from the left side of the screen to the left side of the window.
-			set ll to (item 1 of b)
-			-- The distance in pixels from the top of the screen to the top of the window.
-			set tt to (item 2 of b)
-			-- The distance in pixels from the left side of the screen to the right side of the window.
-			set lr to (item 3 of b)
-			-- The distance in pixels from the top of the screen to the bottom of the window.
-			set tb to (item 4 of b)
-			log "stdout: windowid - bounds: " & (index of theWindow as string) & " -" & ¬
-				" ll=" & (ll as string) & ¬
-				",tt=" & (tt as string) & ¬
-				",lr=" & (lr as string) & ¬
-				",tb=" & (tb as string)
-			
-			try
-				repeat with theTab in tab of theWindow
-					log "stdout: windowid - url: " & (index of theWindow as string) & " - " & (URL of theTab as string)
+printf "DATE: "
+date '+%d.%m.%Y-%H:%M:%S'
+for browser in "${@:-Safari}"; do
+	# Call AppleScript.
+	osascript -e '
+		-- Get the list of running processes
+		tell application "System Events"
+			set listOfRunningProcesses to (name of every process)
+		end tell
+		
+		-- Check the selected browser is currently running
+		if (listOfRunningProcesses contains "'"$browser"'") then
+			log "stdout: BROWSER: '"$browser"'"
+			tell application "'"$browser"'"
+				repeat with theWindow in windows
+					-- For the bounds definition: https://www.macosxautomation.com/applescript/firsttutorial/11.html
+					set b to bounds of theWindow
+					-- The distance in pixels from the left side of the screen to the left side of the window.
+					set ll to (item 1 of b)
+					-- The distance in pixels from the top of the screen to the top of the window.
+					set tt to (item 2 of b)
+					-- The distance in pixels from the left side of the screen to the right side of the window.
+					set lr to (item 3 of b)
+					-- The distance in pixels from the top of the screen to the bottom of the window.
+					set tb to (item 4 of b)
+					log "stdout: windowid - bounds: " & (index of theWindow as string) & " -" & ¬
+						" ll=" & (ll as string) & ¬
+						",tt=" & (tt as string) & ¬
+						",lr=" & (lr as string) & ¬
+						",tb=" & (tb as string)
+					
+					try
+						repeat with theTab in tab of theWindow
+							log "stdout: windowid - url: " & (index of theWindow as string) & " - " & (URL of theTab as string)
+						end repeat
+					on error errMsg
+						log "stderr: error while iterating tab of window named " & (name of theWindow as string) & " in browser '"$browser"': " & errMsg
+					end try
 				end repeat
-			on error errMsg
-				log "stderr: error while iterating tab of window named " & (name of theWindow as string) & ": " & errMsg
-			end try
-		end repeat
-	end tell
-end if
-' 2>&1 | redirect_osascript_output
+			end tell
+		end if
+	' 2>&1 | redirect_osascript_output
+done
