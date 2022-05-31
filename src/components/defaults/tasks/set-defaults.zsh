05ac1a05bde8f15c7f507e627bd1502798c38807
Frizlab <fload@me.com>
2022-05-31 23:52:24 +0200

    Add a small comment regarding a TextEdit default

--- src/components/defaults/tasks/set-defaults.zsh
+++ src/components/defaults/tasks/set-defaults.zsh
@@ -152,6 +152,7 @@ done
 
 ######### TextEdit #########
 
+# It seems type of this one *is* int, not bool.
 start_task "set default document type in TextEdit to text"
 catchout RES  libdefaults__set_int com.apple.TextEdit RichText 0
 log_task_from_res "$RES"
