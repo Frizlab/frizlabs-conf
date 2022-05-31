09f8bec1aaa38b7b243fe101c794a1b43079ab81
Frizlab <fload@me.com>
2022-05-31 17:17:36 +0200

    Install pipenv in work environment

--- src/components/pkgs-brew-system/vars/work.zsh
+++ src/components/pkgs-brew-system/vars/work.zsh
@@ -1,3 +1,7 @@
+PKGS_BREW_SYSTEM__MAIN_FORMULAE+=(
+	"pipenv" "bin/pipenv"
+)
+
 PKGS_BREW_SYSTEM__CASKS+=(
 	"brave-browser" "Caskroom/brave-browser"
 	# Not needed anymore; now it’s installed via Munki
