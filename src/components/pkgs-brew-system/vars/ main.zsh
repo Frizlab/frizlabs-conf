5c81e5c8b314a1a616b1b554b2515c6083dbca33
Frizlab <fload@me.com>
2022-05-31 23:35:28 +0200

    Install fd in system brew

--- src/components/pkgs-brew-system/vars/ main.zsh
+++ src/components/pkgs-brew-system/vars/ main.zsh
@@ -2,6 +2,7 @@
 typeset -A PKGS_BREW_SYSTEM__MAIN_FORMULAE=(
 	"cloc"                "bin/cloc"
 	"coreutils"           "bin/gsort"
+	"fd"                  "bin/fd"
 	"git-lfs"             "bin/git-lfs"
 	"gti"                 "bin/gti"
 	"htop"                "bin/htop"
