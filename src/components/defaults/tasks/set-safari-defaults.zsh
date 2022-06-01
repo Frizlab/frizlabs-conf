eb23429071c7640d8c5126873fb73cbb2a28ce50
Frizlab <fload@me.com>
2022-06-01 20:28:57 +0200

    Dummy alignment fix

--- src/components/defaults/tasks/set-safari-defaults.zsh
+++ src/components/defaults/tasks/set-safari-defaults.zsh
@@ -18,9 +18,9 @@ for b in Safari SafariTechnologyPreview; do
 	log_task_from_res "$RES"
 	
 	start_task "use compact tab layout ($b)"
-	{ res_check "$RES" &&   catchout RES  libdefaults__set_bool  "com.apple.$b" ShowStandaloneTabBar 0                                                                                                                                                                                                             && RES_LIST+=("$RES") }
-	{ res_check "$RES" &&   catchout RES  libdefaults__set_plist "com.apple.$b" "$SAFARI_TOOLBAR_ITEMS_KEY"                                             "$SAFARI_TOOLBAR_CONFIG_ITEMS_VALUE"                                                                                                                       && RES_LIST+=("$RES") } # Might be useless
-	{ res_check "$RES" &&   catchout RES  libdefaults__add_dict  "com.apple.$b" "$SAFARI_TOOLBAR_CONFIG_DEFAULT_KEY" "$SAFARI_TOOLBAR_CONFIG_ITEMS_KEY" "$SAFARI_TOOLBAR_CONFIG_ITEMS_VALUE"                                                                                                                       && RES_LIST+=("$RES") }
+	{ res_check "$RES" &&   catchout RES  libdefaults__set_bool  "com.apple.$b" ShowStandaloneTabBar 0                                                                                                                                                                                                                 && RES_LIST+=("$RES") }
+	{ res_check "$RES" &&   catchout RES  libdefaults__set_plist "com.apple.$b" "$SAFARI_TOOLBAR_ITEMS_KEY"                                             "$SAFARI_TOOLBAR_CONFIG_ITEMS_VALUE"                                                                                                                           && RES_LIST+=("$RES") } # Might be useless
+	{ res_check "$RES" &&   catchout RES  libdefaults__add_dict  "com.apple.$b" "$SAFARI_TOOLBAR_CONFIG_DEFAULT_KEY" "$SAFARI_TOOLBAR_CONFIG_ITEMS_KEY" "$SAFARI_TOOLBAR_CONFIG_ITEMS_VALUE"                                                                                                                           && RES_LIST+=("$RES") }
 	{ res_check "$RES" && { catchout RES  run_and_log_keep_stdout ./lib/update-safari-default-toolbar-for-compact-tab-layout.swift "com.apple.$b" "$SAFARI_TOOLBAR_CONFIG_DEFAULT_KEY" || log_task_failure "error while running update-safari-default-toolbar-for-compact-tab-layout (do you have Xcode installed?)" } && RES_LIST+=("$RES") } # Might be useless
 	log_task_from_res_list RES_LIST
 	
