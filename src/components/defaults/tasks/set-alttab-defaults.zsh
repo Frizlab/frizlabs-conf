5bd12b4a16532251f89ba8f6269b16dc5f71f20e
Frizlab <fload@me.com>
2022-06-01 12:31:51 +0200

    Add some defaults for AltTab

---
+++ src/components/defaults/tasks/set-alttab-defaults.zsh
@@ -0,0 +1,25 @@
+# Setup some defaults for AltTab
+
+start_task "set show windows from visible spaces for shortcut 1 in AltTab"
+catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos spacesToShow "1"
+log_task_from_res "$RES"
+
+start_task "set show windows from screen showing AltTab for shortcut 1 in AltTab"
+catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos screensToShow "1"
+log_task_from_res "$RES"
+
+start_task "set show minimized windows at the end for shortcut 1 in AltTab"
+catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos showMinimizedWindows "2"
+log_task_from_res "$RES"
+
+start_task "set show hidden windows at the end for shortcut 1 in AltTab"
+catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos showHiddenWindows "2"
+log_task_from_res "$RES"
+
+start_task "remove shortcut 2 in AltTab"
+catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos nextWindowShortcut2 ""
+log_task_from_res "$RES"
+
+start_task "set apparition delay in AltTab"
+catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos windowDisplayDelay "185"; #ms
+log_task_from_res "$RES"
