3cad296f6b116ea3e763dbaf33095e575d84f1bf
Frizlab <fload@me.com>
2022-06-01 15:37:48 +0200

    Set shortcut to go to previous window in AltTab

--- src/components/defaults/tasks/set-alttab-defaults.zsh
+++ src/components/defaults/tasks/set-alttab-defaults.zsh
@@ -1,5 +1,8 @@
 # Setup some defaults for AltTab
 
+
+### Controls ###
+
 start_task "set show windows from visible spaces for shortcut 1 in AltTab"
 catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos spacesToShow "1"
 log_task_from_res "$RES"
@@ -16,9 +19,16 @@ start_task "set show hidden windows at the end for shortcut 1 in AltTab"
 catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos showHiddenWindows "2"
 log_task_from_res "$RES"
 
+start_task "set select previous window shortcut for shortcut 1 in AltTab"
+catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos previousWindowShortcut $'\U2325\U21e7\U21e5'
+log_task_from_res "$RES"
+
 start_task "remove shortcut 2 in AltTab"
 catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos nextWindowShortcut2 ""
 log_task_from_res "$RES"
+
+
+### Appearance ###
 
 start_task "set apparition delay in AltTab"
 catchout RES  libdefaults__set_str com.lwouis.alt-tab-macos windowDisplayDelay "185"; #ms
