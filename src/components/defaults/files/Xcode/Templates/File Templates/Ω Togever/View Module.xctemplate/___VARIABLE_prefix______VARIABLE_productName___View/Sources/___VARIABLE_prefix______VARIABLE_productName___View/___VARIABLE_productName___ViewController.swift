fe0d2ac885b2c64432cf357606a7563f1e592241
Frizlab <fload@me.com>
2022-06-04 16:30:25 +0200

    Fix a few incorrect definitions in the View Module template

--- src/components/defaults/files/Xcode/Templates/File Templates/Ω Togever/View Module.xctemplate/___VARIABLE_prefix______VARIABLE_productName___View/Sources/___VARIABLE_prefix______VARIABLE_productName___View/___VARIABLE_productName___ViewController.swift
+++ src/components/defaults/files/Xcode/Templates/File Templates/Ω Togever/View Module.xctemplate/___VARIABLE_prefix______VARIABLE_productName___View/Sources/___VARIABLE_prefix______VARIABLE_productName___View/___VARIABLE_productName___ViewController.swift
@@ -9,17 +9,17 @@ import UIKit
 @MainActor
 public final class ___FILEBASENAMEASIDENTIFIER___ : UIViewController {
 	
-	public static func instantiate(viewModel: ___VARIABLE_prefix______VARIABLE_productName___ViewModel, coordinator: ___VARIABLE_prefix______VARIABLE_productName___Coordinator) -> ___FILEBASENAMEASIDENTIFIER___ {
+	public static func instantiate(viewModel: ___VARIABLE_productName___ViewModel, coordinator: ___VARIABLE_productName___Coordinator) -> ___FILEBASENAMEASIDENTIFIER___ {
 		return UIStoryboard(name: "___FILEBASENAMEASIDENTIFIER___", bundle: .module)
 			.instantiateInitialViewController{ coder in
 				___FILEBASENAMEASIDENTIFIER___(coder: coder, viewModel: viewModel, coordinator: coordinator)
 			}!
 	}
 	
-	public let viewModel: ___VARIABLE_prefix______VARIABLE_productName___ViewModel
-	public private(set) weak var coordinator: ___VARIABLE_prefix______VARIABLE_productName___Coordinator?
+	public let viewModel: ___VARIABLE_productName___ViewModel
+	public private(set) weak var coordinator: ___VARIABLE_productName___Coordinator?
 	
-	public init?(coder: NSCoder, viewModel: ___VARIABLE_prefix______VARIABLE_productName___ViewModel, coordinator: ___VARIABLE_prefix______VARIABLE_productName___Coordinator?) {
+	public init?(coder: NSCoder, viewModel: ___VARIABLE_productName___ViewModel, coordinator: ___VARIABLE_productName___Coordinator?) {
 		self.viewModel = viewModel
 		self.coordinator = coordinator
 		
