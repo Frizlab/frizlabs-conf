56b8bda4e92e0ce555e52d330b8a7c8f007af6c1
Frizlab <fload@me.com>
2022-07-21 15:36:14 +0200

    Show Safari status bar

--- src/components/defaults/tasks/set-safari-defaults.zsh
+++ src/components/defaults/tasks/set-safari-defaults.zsh
@@ -17,6 +17,10 @@ for b in Safari SafariTechnologyPreview; do
 	catchout RES  libdefaults__set_bool "com.apple.$b" EnableNarrowTabs 0
 	log_task_from_res "$RES"
 	
+	start_task "$b: show status bar"
+	catchout RES  libdefaults__set_bool "com.apple.$b" ShowOverlayStatusBar 1
+	log_task_from_res "$RES"
+	
 	# We used to have a very complicated setup here where we tried and anticipate the changes to the toolbar that the show standalone tab bar setting entails.
 	# However after some testing it seems changing this single key is enough, and the rest follows whenever it wants.
 	start_task "$b: use separate tab layout"
