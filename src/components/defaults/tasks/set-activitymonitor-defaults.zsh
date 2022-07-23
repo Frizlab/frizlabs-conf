574a22b754d159589d92c2812d63bb65c0ae30bc
Frizlab <fload@me.com>
2022-07-23 17:19:54 +0200

    Add new default for activity monitor

--- src/components/defaults/tasks/set-activitymonitor-defaults.zsh
+++ src/components/defaults/tasks/set-activitymonitor-defaults.zsh
@@ -7,3 +7,7 @@ log_task_from_res "$RES"
 start_task "Activity Monitor: set update frequency"
 catchout RES  libdefaults__set_int com.apple.ActivityMonitor UpdatePeriod 2
 log_task_from_res "$RES"
+
+start_task "Activity Monitor: show percent of parent in samples"
+catchout RES  libdefaults__set_int com.apple.ActivityMonitor DisplayType 3
+log_task_from_res "$RES"
