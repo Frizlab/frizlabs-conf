3f408a089dfdecf20f824566b53d2cb69d13ce8a
Frizlab <fload@me.com>
2022-07-23 23:31:32 +0200

    Add a comment in the character picker defaults action

--- src/components/defaults/tasks/set-characterpicker-defaults.zsh
+++ src/components/defaults/tasks/set-characterpicker-defaults.zsh
@@ -1,6 +1,7 @@
 # Setup some defaults for the Character Picker
 
 start_task "Character Picker: set the favorite characters"
+# Note: We have left the Unicode escapes of the characters but we could definitely replace them with the actual characters (e.g. replace “\\U03a9” with “Ω”).
 catchout RES  libdefaults__set_plist com.apple.CharacterPicker Favorites:com.apple.CharacterPicker.DefaultDataStorage "
 	(
 		\"com.apple.cpk:{\\\"char\\\":\\\"\\U03a9\\\"}\", /* Ω */
