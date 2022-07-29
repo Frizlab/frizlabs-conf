039f169e41a0ef075666749e737b997112b35ffb
Frizlab <fload@me.com>
2022-07-29 19:46:20 +0200

    Add shortcut override for Slow Animations menu entry in the iPhone Simulator

---
+++ src/components/defaults/tasks/set-iphonesimulator-defaults.zsh
@@ -0,0 +1,9 @@
+# Setup some defaults for the iPhone Simulator
+
+start_task "iPhone Simulator: add user-defined key-equivalents"
+catchout RES  libdefaults__set_plist com.apple.iphonesimulator NSUserKeyEquivalents "
+	{
+		\"Slow Animations\" = \"@^s\";
+	}
+"
+log_task_from_res "$RES"
