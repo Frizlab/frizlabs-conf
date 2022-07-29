338e392097a625a4418d6a91ef4fc35fc48a0a71
Frizlab <fload@me.com>
2022-07-29 19:50:30 +0200

    Add Stay On Top shortcut for the iPhone Simulator

--- src/components/defaults/tasks/set-iphonesimulator-defaults.zsh
+++ src/components/defaults/tasks/set-iphonesimulator-defaults.zsh
@@ -3,6 +3,7 @@
 start_task "iPhone Simulator: add user-defined key-equivalents"
 catchout RES  libdefaults__set_plist com.apple.iphonesimulator NSUserKeyEquivalents "
 	{
+		\"Stay On Top\"     = \"@^t\";
 		\"Slow Animations\" = \"@^s\";
 	}
 "
