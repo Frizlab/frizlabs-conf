f102fb35662c865924ee239238e2e0e45b1db1e4
Frizlab <fload@me.com>
2022-10-04 21:30:09 +0200

    Add more domains to the dark mode whitelist

--- src/components/defaults/vars/work.zsh
+++ src/components/defaults/vars/work.zsh
@@ -4,6 +4,7 @@ DEFAULTS__XCODE_THEME_ACTIONS+=(
 )
 
 DEFAULTS__DARK_MODE_FOR_SAFARI_WHITELISTED_SITES+=(
+	"alertmanager-prod.1e42.net"
 	"happn.pagerduty.com"
 	"happnapp.atlassian.net"
 	"office.1e42.net"
