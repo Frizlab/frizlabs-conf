78710938bb20f5ea5925b1d02cf34e1e22752ab3
Frizlab <fload@me.com>
2022-11-19 13:28:50 +0100

    Use better task title for decrypt task

--- src/components/dotfiles/tasks/decrypts.zsh
+++ src/components/dotfiles/tasks/decrypts.zsh
@@ -2,7 +2,7 @@
 for file in "${DOTFILES__ENCRYPTED[@]}"; do
 	dest_file="$HOME/$file"
 	repo_file="$COMPONENT_ROOT_FOLDER/files/_$file.cpt"
-	start_task "decrypt _$file -> ${dest_file/#$HOME/\~}"
+	start_task "decrypt ${dest_file/#$HOME/\~}"
 	catchout RES   libfiles__decrypt_and_copy "$repo_file" "$dest_file" "600" "$DOTFILES__BACKUP_DIR" "$DOTFILES__BACKUP_DIR_MODE"
 	log_task_from_res "$RES"
 done
