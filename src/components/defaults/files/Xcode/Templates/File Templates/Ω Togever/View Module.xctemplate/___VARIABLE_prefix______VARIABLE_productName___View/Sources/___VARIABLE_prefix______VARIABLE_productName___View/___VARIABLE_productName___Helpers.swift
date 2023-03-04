17cf9cd9c7621fd0ed24a3bbaebf7a021139b8db
Frizlab <fload@me.com>
2023-03-04 13:07:36 +0100

    Update the “View Module” template for Xcode

---
+++ src/components/defaults/files/Xcode/Templates/File Templates/Ω Togever/View Module.xctemplate/___VARIABLE_prefix______VARIABLE_productName___View/Sources/___VARIABLE_prefix______VARIABLE_productName___View/___VARIABLE_productName___Helpers.swift
@@ -0,0 +1,26 @@
+//___FILEHEADER___
+
+import Combine
+import Foundation
+
+import CoordinatorProtocol
+
+
+
+/* *******************
+   MARK: - Coordinator
+   ******************* */
+
+@MainActor
+public protocol ___VARIABLE_productName___Coordinator : Coordinator {
+}
+
+
+
+/* *****************
+   MARK: - ViewModel
+   ***************** */
+
+@MainActor
+public protocol ___VARIABLE_productName___ViewModel {
+}
