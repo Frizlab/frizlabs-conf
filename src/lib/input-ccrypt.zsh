fcfc0d83c3eff48b2f5b3dcaa4859044dd0f1904
Frizlab <fload@me.com>
2023-05-23 15:46:30 +0200

    Use proper address for ccrypt and follow redirects

--- src/lib/input-ccrypt.zsh
+++ src/lib/input-ccrypt.zsh
@@ -9,8 +9,8 @@
 	readonly CCRYPT_VERSION="1.11"
 	readonly CCRYPT_BASENAME="ccrypt-$CCRYPT_VERSION"
 	readonly CCRYPT_TAR_NAME="$CCRYPT_BASENAME.tar.gz"
-	readonly CCRYPT_URL="http://ccrypt.sourceforge.net/download/$CCRYPT_VERSION/$CCRYPT_TAR_NAME"
-	test -e "$CCRYPT_TAR_NAME" || curl "$CCRYPT_URL" >"$CCRYPT_TAR_NAME"
+	readonly CCRYPT_URL="https://ccrypt.sourceforge.net/download/$CCRYPT_VERSION/$CCRYPT_TAR_NAME"
+	test -e "$CCRYPT_TAR_NAME" || curl --location "$CCRYPT_URL" >"$CCRYPT_TAR_NAME"
 	test "$(shasum "$CCRYPT_TAR_NAME" | cut -d' ' -f1)" = "$CCRYPT_SHASUM" || {
 		echo "***** ERROR: ccrypt sha does not match expected sha. Bailing out."
 		exit 1
