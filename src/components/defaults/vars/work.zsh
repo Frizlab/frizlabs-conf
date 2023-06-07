29e749f49551aea5bcaf3ee006a395d02c4edbdd
Frizlab <fload@me.com>
2023-06-07 22:05:02 +0200

    Update Dark Mode whitelist

--- src/components/defaults/vars/work.zsh
+++ src/components/defaults/vars/work.zsh
@@ -6,6 +6,7 @@ DEFAULTS__XCODE_THEME_ACTIONS+=(
 DEFAULTS__DARK_MODE_FOR_SAFARI_WHITELISTED_SITES+=(
 	"alertmanager-prod.1e42.net"
 	"console.happn.app"
+	"drive.happn.private"
 	"happn.pagerduty.com"
 	"happnapp.atlassian.net"
 	"office.1e42.net"
