9baf0dbb721ef5529e8ec3a546103ccd045a9141
Frizlab <fload@me.com>
2023-06-16 18:50:05 +0200

    Allow deff to diff full system defaults instead of just

--- src/components/bin/files/sh/deff.sh
+++ src/components/bin/files/sh/deff.sh
@@ -6,7 +6,7 @@ set -eu
 # First run will always show an empty diff (the initial state will be saved in the Caches directory).
 # Next run, if the defaults have been changed, the diff will be shown.
 
-readonly BUNDLE_ID="$1"; shift
+readonly BUNDLE_ID="${1:-}"
 
 
 readonly CACHES_FOLDER="$HOME/Library/Caches/me.frizlab.deff"
@@ -19,10 +19,15 @@ readonly  AFTER_PATH="$CACHES_FOLDER/$BUNDLE_ID-after.txt"
 # Is it the first run for this bundle id?
 if test ! -e "$AFTER_PATH"; then
 	# First run, we simply store the current state.
-	defaults read "$BUNDLE_ID" >"$AFTER_PATH"
+	#
+	# Note:
+	# NO quotes around the BUNDLE_ID variable as we do want to list all the defaults if an empty bundle ID is given.
+	# We assume all valid bundle IDs will never need quoting.
+	# We do this because we want to avoid an if haha.
+	defaults read $BUNDLE_ID >"$AFTER_PATH"
 else
 	# Not first run, we get the new state and diff.
 	mv -f -- "$AFTER_PATH" "$BEFORE_PATH"
-	defaults read "$BUNDLE_ID" >"$AFTER_PATH"
+	defaults read $BUNDLE_ID >"$AFTER_PATH"
 	diff -- "$BEFORE_PATH" "$AFTER_PATH"
 fi
