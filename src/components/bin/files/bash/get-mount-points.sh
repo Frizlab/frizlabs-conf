df4d9f0c696278e8db5058a2e6e9ac9373b49f9d
Frizlab <fload@me.com>
2023-10-01 07:35:09 +0200

    Dummy whitespace changes

--- src/components/bin/files/bash/get-mount-points.sh
+++ src/components/bin/files/bash/get-mount-points.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+
 if [ -z "$1" ]; then
 	echo "Usage: $0 image_disk" >&2
 	exit 42
 fi
+
 
 if [ ! -e "$1" ]; then
 	echo "$0: $1: No such file or directory" >&2
@@ -16,17 +18,17 @@ hdiutil info | awk '
 	BEGIN {
 		FS=": ";
 	}
-
+	
 	/================================================/ {
 		cur_image_path = "";
 		FS=": ";
 	}
-
+	
 	/image-path/ {
 		image_path = $2;
 		FS="\t";
 	}
-
+	
 	/^\/dev\/.*/ {
 		if (image_path == "'"$di"'") {
 			if ($3 != "") print $3;
