816661a6666a1dabc7be7fe963c827b92e331f5b
Frizlab <fload@me.com>
2023-12-08 12:02:47 +0100

    Remove Packer and Viscosity from work stuff

--- src/components/pkgs-brew-system/vars/work.zsh
+++ src/components/pkgs-brew-system/vars/work.zsh
@@ -1,6 +1,5 @@
 PKGS_BREW_SYSTEM__MAIN_FORMULAE+=(
 	"just"   "bin/just"
-	"packer" "bin/packer"
 	"pipenv" "bin/pipenv"
 	"sops"   "bin/sops"
 )
@@ -8,5 +7,4 @@ PKGS_BREW_SYSTEM__MAIN_FORMULAE+=(
 PKGS_BREW_SYSTEM__CASKS+=(
 	"brave-browser" "Caskroom/brave-browser"
 	"firefox"       "Caskroom/firefox"
-	"viscosity"     "Caskroom/viscosity"
 )
