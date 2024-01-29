d021a2c9f7198c9906964e15ad6c322028d32021
Frizlab <fload@me.com>
2024-01-29 14:45:09 +0100

    Fix and modernize run-on-change

--- src/components/bin/files/swift-sh/run-on-change.swift
+++ src/components/bin/files/swift-sh/run-on-change.swift
@@ -1,65 +1,66 @@
 #!/usr/bin/env -S swift-sh --
 import Foundation
+#if canImport(System)
+import System
+#else
+import SystemPackage     /* @apple/swift-system                     ~> 1.0.0 */
+#endif
 
-import ArgumentParser  /* @apple/swift-argument-parser ~> 1.2.0 */
-import FSEventsWrapper /* @Frizlab                     ~> 1.0.2 */
-import SwiftShell      /* @kareman                     ~> 5.1.0 */
+import ArgumentParser    /* @apple/swift-argument-parser            ~> 1.3.0 */
+import AsyncAlgorithms   /* @apple/swift-async-algorithms           ~> 1.0.0 */
+import CLTLogger         /* @xcode-actions/clt-logger               ~> 0.8.0 */
+import FSEventsWrapper   /* @Frizlab                                ~> 2.1.0 */
+import Logging           /* @apple/swift-log                        ~> 1.5.3 */
+import ProcessInvocation /* @xcode-actions/swift-process-invocation ~> 1.0.0-beta */
 
 
+
+/* Let’s bootstrap the logger first and make it available globally. */
+LoggingSystem.bootstrap { _ in CLTLogger() }
+let logger: Logger = {
+	var ret = Logger(label: "main")
+	ret.logLevel = .info
+	return ret
+}()
+
+ProcessInvocationConfig.logger = nil
 
 _ = await Task{ await Main.main() }.value
 struct Main : AsyncParsableCommand {
 	
-	@Argument(help: "The path to monitor")
+	@Option(name: .shortAndLong, help: #"The shell to use for running the script. The shell will be run like so:\#n   $shell -c "$command""#)
+	var shell: String = "zsh"
+	
+	@Option(name: .shortAndLong, help: "The minimum interval of time that has to happen between two launches of the script.")
+	var throttleMilliseconds: String = "zsh"
+	
+	@Argument(help: "The path to monitor.")
 	var path: String
 	
-	@Argument(help: "The command to run")
+	@Argument(help: "The script to run.")
 	var command: String
 	
-	private enum CodingKeys: CodingKey {
-		case path
-		case command
-	}
-	
-	func run() throws {
-		let callback = { (stream: FSEventStream, event: FSEvent) in
-//			NSLog("Got FSEvent: %@", String(describing: event))
-			dispatcher.dispatchCommand(command: command)
+	func run() async throws {
+		logger.info("Starting path observation.")
+		let fsEventsStream = FSEventAsyncStream(path: path, flags: FSEventStreamCreateFlags(kFSEventStreamCreateFlagFileEvents))
+		for await event in fsEventsStream._throttle(for: .milliseconds(250)) {
+			logger.debug("Got new FSEvent.", metadata: ["event": "\(event)"])
+			logger.info("Executing command.", metadata: ["date": "\(Date())"])
+			do {
+				_ = try await ProcessInvocation(FilePath(shell), "-c", command, stdoutRedirect: .none, stderrRedirect: .none).invokeAndGetRawOutput()
+				logger.info("Command completed successfully.")
+			} catch {
+				logger.info("Command exited w/ error.", metadata: ["error": "\(error)"])
+			}
 		}
 		
-		struct SimpleError : Error {var message: String}
-		guard let w = FSEventStream(path: path, fsEventStreamFlags: FSEventStreamCreateFlags(kFSEventStreamCreateFlagFileEvents), callback: callback) else {
-			throw SimpleError(message: "Cannot monitor the given path.")
-		}
-		w.startWatching()
-		
-		repeat {
-			RunLoop.main.run(mode: .default, before: Date(timeIntervalSinceNow: 0.1))
-		} while true
+		/* If we reach this point, there has been an error setting up FSEvent. */
+		throw SimpleError(message: "Cannot monitor the given path.")
 	}
 	
-	var dispatcher = Dispatcher()
-	
-	class Dispatcher {
-		
-		var isWaitingForDispatch = false
-		
-		func dispatchCommand(command: String) {
-			guard !isWaitingForDispatch else {return}
-			isWaitingForDispatch = true
-			
-			DispatchQueue.global().asyncAfter(deadline: .now() + .milliseconds(250), execute: {
-				DispatchQueue.main.sync{ self.isWaitingForDispatch = false }
-				NSLog("Executing command: \(command)")
-				do {
-					try runAndPrint(bash: command)
-					NSLog("Command completed successfully")
-				} catch {
-					NSLog("Command exited w/ error: \(error)")
-				}
-			})
-		}
-		
-	}
-	
+}
+
+
+struct SimpleError : Error {
+	var message: String
 }
