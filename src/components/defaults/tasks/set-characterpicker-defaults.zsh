47f7ddc18a7cc9eadd616e5d8e65a75c82a5d328
Frizlab <fload@me.com>
2024-04-30 17:30:46 +0200

    Add new favorite characters

--- src/components/defaults/tasks/set-characterpicker-defaults.zsh
+++ src/components/defaults/tasks/set-characterpicker-defaults.zsh
@@ -13,12 +13,14 @@ catchout RES  libdefaults__set_plist com.apple.CharacterPicker Favorites:com.app
 		\"com.apple.cpk:{\\\"char\\\":\\\"\\U021b\\\"}\", /* ț */
 		\"com.apple.cpk:{\\\"char\\\":\\\"\\U03bc\\\"}\", /* μ (lowercase mu, official abbreviation for “micro-”). Not to be confused with µ (U+00B5), which is the legacy codepoint for the same thing. See <https://en.wikipedia.org/wiki/Micro->. */
 		\"com.apple.cpk:{\\\"char\\\":\\\"→\\\"}\",       /* Sub-section MARK prefix in Xcode. Also quite simply the best right arrow there is. */
+		\"com.apple.cpk:{\\\"char\\\":\\\"×\\\"}\",       /* The multiplication sign. */
 		\"com.apple.cpk:{\\\"char\\\":\\\"₋\\\"}\",       /* Separator for the “gender me”    group of XibLoc. */
 		\"com.apple.cpk:{\\\"char\\\":\\\"¦\\\"}\",       /* Separator for the “gender other” group of XibLoc. */
 		\"com.apple.cpk:{\\\"char\\\":\\\"↔\\\"}\",       /* Best left and right arrow, sometimes used in filenames in my projects. */
 		\"com.apple.cpk:{\\\"char\\\":\\\"←\\\"}\",       /* Best left arrow. */
 		\"com.apple.cpk:{\\\"char\\\":\\\"↑\\\"}\",       /* Best up   arrow. */
 		\"com.apple.cpk:{\\\"char\\\":\\\"↓\\\"}\",       /* Best down arrow. */
+		\"com.apple.cpk:{\\\"char\\\":\\\"𝓕\\\"}\",       /* My brand, kind of :) */
 		\"com.apple.cpk:{\\\"char\\\":\\\"⍼\\\"}\"        /* Right angle with downwards zigzag arrow. See <https://ionathan.ch/2022/04/09/angzarr.html>. */
 	)
 "
