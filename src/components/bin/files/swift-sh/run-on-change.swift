b1b90849b9c2e9ad78ac58ab7483ccac89e3cade
Frizlab <fload@me.com>
2025-01-24 21:54:34 +0100

    Fix processing of events while command is running for run-on-change

--- src/components/bin/files/swift-sh/run-on-change.swift
+++ src/components/bin/files/swift-sh/run-on-change.swift
@@ -41,21 +41,64 @@ struct RunOnChange : AsyncParsableCommand {
 	var command: String
 	
 	func run() async throws {
+		let runningState = RunningState()
+		
 		logger.info("Starting path observation.")
 		let fsEventsStream = FSEventAsyncStream(path: path, flags: FSEventStreamCreateFlags(kFSEventStreamCreateFlagFileEvents))
 		for await event in fsEventsStream._throttle(for: .milliseconds(throttleMilliseconds)) {
 			logger.debug("Got new FSEvent.", metadata: ["event": "\(event)"])
-			logger.info("Executing command.", metadata: ["date": "\(Date())"])
-			do {
-				_ = try await ProcessInvocation(FilePath(shell), "-c", command, stdoutRedirect: .none, stderrRedirect: .none).invokeAndGetRawOutput()
-				logger.info("Command completed successfully.")
-			} catch {
-				logger.info("Command exited w/ error.", metadata: ["error": "\(error)"])
-			}
+			Task{ await runCommand(runningState: runningState) }
 		}
 		
 		/* If we reach this point, there has been an error setting up FSEvent. */
 		throw SimpleError(message: "Cannot monitor the given path.")
+	}
+	
+	func runCommand(runningState: RunningState) async {
+		guard await runningState.switchToRunningNow() else {
+			logger.debug("Deferring command execution due to previous command still running.", metadata: ["date": "\(Date())"])
+			await runningState.needsRunningNext()
+			return
+		}
+		
+		do {
+			logger.info("Executing command.", metadata: ["date": "\(Date())"])
+			_ = try await ProcessInvocation(FilePath(shell), "-c", command, stdinRedirect: .none(setFgPgID: true), stdoutRedirect: .none, stderrRedirect: .none).invokeAndGetRawOutput()
+			logger.info("Command completed successfully.")
+		} catch {
+			logger.info("Command exited w/ error.", metadata: ["error": "\(error)"])
+		}
+		
+		let runningNow = await runningState.runningNow
+		assert(runningNow)
+		
+		if await runningState.finishRunning() {
+			await runCommand(runningState: runningState)
+		}
+	}
+	
+	actor RunningState {
+		
+		var runningNow: Bool = false
+		var runningNext: Bool = false
+		
+		func switchToRunningNow() -> Bool {
+			let wasRunning = runningNow
+			runningNow = true
+			return !wasRunning
+		}
+		
+		func finishRunning() -> Bool {
+			let needsRunningNext = runningNext
+			runningNow = false
+			runningNext = false
+			return needsRunningNext
+		}
+		
+		func needsRunningNext() {
+			runningNext = true
+		}
+		
 	}
 	
 }
