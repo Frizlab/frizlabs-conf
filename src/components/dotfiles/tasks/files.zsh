786db906223b6863e5d04820cf1f26dc9a99cd4f
Frizlab <fload@me.com>
2025-02-18 14:41:41 +0100

    Split gh config between work and home

--- src/components/dotfiles/tasks/files.zsh
+++ src/components/dotfiles/tasks/files.zsh
@@ -1,7 +1,10 @@
 # Create links to files in this repo, with backup if needed.
 for file in "${DOTFILES__FILES[@]}"; do
 	dest_file="$HOME/$file"
-	repo_file="$COMPONENT_ROOT_FOLDER/files/_$file"
+	# Find the correct destination link.
+	# If the file exists with an "@$COMPUTER_GROUP" suffix before the extension, we take this value.
+	repo_file="$COMPONENT_ROOT_FOLDER/files/_${file%.*}@$COMPUTER_GROUP.${file##*.}"
+	test -f "$repo_file" || repo_file="$COMPONENT_ROOT_FOLDER/files/_$file"
 	start_task "link _$file -> ${dest_file/#$HOME/\~}"
 	catchout RES   libfiles__linknbk "$repo_file" "$dest_file" "600" "$DOTFILES__BACKUP_DIR" "$DOTFILES__BACKUP_DIR_MODE"
 	log_task_from_res "$RES"
